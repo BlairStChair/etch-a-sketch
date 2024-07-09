2ad0d07637f79a591f341ae7143c48818b93a6a3
BlairStChair <julia.z15@interia.pl>
2024-07-09 23:02:45 +0200

    Added function to create new grid after clicking the button

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ for(var i = 0; i < 256; i++){
 
 newGridBtn.addEventListener("click", () => {
     const gridSize = prompt("Choose a size of your canvas (maximum size is 100x100)");
-    
+
     document.querySelectorAll(".newDivClass").forEach(newDiv => newDiv.remove());
+
+    for(var i = 0; i < gridSize*gridSize; i++){
+        const customDiv = document.createElement("div");
+        customDiv.id = "div" + i;
+        customDiv.className = "customDivClass";
+        customDiv.style.flex = `0 0 calc(100% / (${gridSize} + 1))`;
+        container.appendChild(customDiv);
+    
+        customDiv.addEventListener("mouseover", () => {
+            customDiv.style.backgroundColor = "green";
+            
+        });
+    }
+    
 });
 
